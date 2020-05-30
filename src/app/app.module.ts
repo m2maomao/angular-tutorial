2e98c33d404c462a764a67600085cef59ab6ad64
m2maomao <271291823@qq.com>
2020-05-30 23:18:40 +0800

    2 files modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeroChildComponent } from './hero-child/hero-child.component';
+import { HeroParentComponent } from './hero-parent/hero-parent.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, HeroChildComponent, HeroParentComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
--- src/app/hero.ts
+++ src/app/hero.ts
@@ -3,3 +3,10 @@ export class Hero {
     public id: number,
     public name: string) { }
 }
+export const HEROES = [
+  { id: 1, name: 'Michael'},
+  { id: 2, name: 'John'},
+  { id: 3, name: 'Kevin'},
+  { id: 4, name: 'Tom'},
+  { id: 5, name: 'Jerry'}
+]
