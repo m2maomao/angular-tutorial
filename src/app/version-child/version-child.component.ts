9f86d1bbe4f2c0ab81241e77bab8ebac78f48b46
m2maomao <271291823@qq.com>
2020-05-31 01:19:39 +0800

    2 files modified

--- src/app/version-child/version-child.component.ts
+++ src/app/version-child/version-child.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, SimpleChange } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChange } from '@angular/core';
 
 @Component({
   selector: 'app-version-child',
@@ -7,14 +7,23 @@ import { Component, Input, OnChanges, SimpleChange } from '@angular/core';
 })
 export class VersionChildComponent implements OnChanges {
 
+  ngOnInit() {
+    this.name = 'Maomao';
+    console.log('初始化')
+  }
+
   @Input() major: number;
   @Input() minor: number;
+  name: string;
   changeLog: string[] = [];
 
   ngOnChanges(changes: {[propKey: string]: SimpleChange}) {
+    console.log('changes:', changes)
     let log: string[] = [];
     for(let propName in changes) {
+      console.log('propName:', propName)
       let changedProp = changes[propName];
+      console.log('changedProp:', changedProp)
       let to = JSON.stringify(changedProp.currentValue);
       if(changedProp.isFirstChange()) {
         log.push(`Initial value of ${propName} set to ${to}`);
